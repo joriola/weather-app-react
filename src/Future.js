33b37b49a0eb7b34ff1f40fdac4110a62c220dd2
jerpelo <jasmine.erpelo@gmail.com>
2022-02-17 17:09:44 -0500

    added columns for future forecast

--- src/Future.js
+++ src/Future.js
@@ -2,5 +2,69 @@ import React from "react";
 import "./Future.css";
 
 export default function Future() {
-  return <div className="Future">future</div>;
+  let futureData = {
+    city: "New York",
+    day: "Tues",
+    time: "10:00",
+    description: "cloudy",
+    high: 10,
+    low: 2,
+    humidity: 10,
+    wind: 3,
+  };
+  return (
+    <div className="Future">
+      future
+      <div className="row">
+        <div className="col">
+          <img src="/" alt={futureData.description} />
+          <div className="day">{futureData.day}</div>
+          <div className="temp">
+            <span className="high">{futureData.high}°</span>
+            <span className="low"> | {futureData.low}°</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src="/" alt={futureData.description} />
+          <div className="day">{futureData.day}</div>
+          <div className="temp">
+            <span className="high">{futureData.high}°</span>
+            <span className="low"> | {futureData.low}°</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src="/" alt={futureData.description} />
+          <div className="day">{futureData.day}</div>
+          <div className="temp">
+            <span className="high">{futureData.high}°</span>
+            <span className="low"> | {futureData.low}°</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src="/" alt={futureData.description} />
+          <div className="day">{futureData.day}</div>
+          <div className="temp">
+            <span className="high">{futureData.high}°</span>
+            <span className="low"> | {futureData.low}°</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src="/" alt={futureData.description} />
+          <div className="day">{futureData.day}</div>
+          <div className="temp">
+            <span className="high">{futureData.high}°</span>
+            <span className="low"> | {futureData.low}°</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src="/" alt={futureData.description} />
+          <div className="day">{futureData.day}</div>
+          <div className="temp">
+            <span className="high">{futureData.high}°</span>
+            <span className="low"> | {futureData.low}°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
